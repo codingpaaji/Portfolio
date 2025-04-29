ad190a79d329e0066d033baf930aa6e8c0a546a6
Anjali Sharma <anjalisharma4723068@gmail.com>
2025-04-29 17:42:52 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -29,3 +29,35 @@ window.onload = () => {
       });
     }
   };
+  for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all available images
+  }
+};
+
+//fullscreen image preview function
+//selecting all required elements
+const previewBox = document.querySelector(".preview-box"),
+  categoryName = previewBox.querySelector(".title p"),
+  previewImg = previewBox.querySelector("img"),
+  closeIcon = previewBox.querySelector(".icon"),
+  shadow = document.querySelector(".shadow");
+redirectBtn = previewBox.querySelector(".redirect-btn"); // Select the new button
+
+function preview(element) {
+  //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+  let redirectLink = element.getAttribute("data-link");
+  previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
+  categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
+  redirectBtn.href = redirectLink;
+  previewBox.classList.add("show"); //show the preview image box
+  shadow.classList.add("show"); //show the light grey background
+  closeIcon.onclick = () => {
+    //if user click on close icon of preview box
+    previewBox.classList.remove("show"); //hide the preview box
+    shadow.classList.remove("show"); //hide the light grey background
+    document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+  };
+}
